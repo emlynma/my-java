f305b984f138efe4bd2e838223a1ea4b7e574eb8
Emlyn Ma <emlyn.ma@gmail.com>
2023-12-11 05:57:39 +0800

    sort

---
+++ my-java-base/src/main/java/com/emlyn/ma/base/util/AlgorithmUtils.java
@@ -0,0 +1,43 @@
+package com.emlyn.ma.base.util;
+
+public abstract class AlgorithmUtils {
+
+    public static int[] generateSortedIntArray(int count, boolean asc) {
+        int[] array = new int[count];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+        return array;
+    }
+
+    public static int[] generateRandomIntArray(int max, int count) {
+        int[] array = new int[count];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * max);
+        }
+        return array;
+    }
+
+    public static int[] generateAscIntArray(int count) {
+        int[] array = new int[count];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+            if ((i + 1) % 7 == 0) {
+                array[i] = array[i - 2];
+            }
+        }
+        return array;
+    }
+
+    public static int[] generateDescIntArray(int count) {
+        int[] array = new int[count];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array.length - i;
+            if ((i + 1) % 7 == 0) {
+                array[i] = array[i - 2];
+            }
+        }
+        return array;
+    }
+
+}
