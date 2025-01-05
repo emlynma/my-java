9cafa217935ce98f9cec65f40cd800310d33858d
Emlyn Ma <emlyn.ma@gmail.com>
2025-01-06 03:03:22 +0800

    feat: binary search

--- my-java-algo/src/main/java/com/emlynma/java/algo/search/BinarySearch.java
+++ my-java-algo/src/main/java/com/emlynma/java/algo/search/BinarySearch.java
@@ -1,54 +1,57 @@
 package com.emlynma.java.algo.search;
 
 public class BinarySearch {
-    public static void main(String[] args) {
-        int[] array = new int[] {1,2,3,5,6,8,9};
-//        int index = binarySearch(array, 7);
-//        System.out.println(index == -1);
-        int index = binarySearch2(array, 3);
-        System.out.println(index == 2);
-    }
 
-    // iteration
-    private static int binarySearch(int[] array, int target) {
+    // 迭代实现
+    private int search_iteration(int[] array, int target) {
+        // 准入校验
         if (array == null || array.length == 0) {
             return -1;
         }
-        int i = 0, j = array.length - 1;
+        int i = 0; int j = array.length - 1;
         while (i <= j) {
-            int m = i + (j - i) / 2;
+            int m = (i + j) / 2;
             if (array[m] == target) {
                 return m;
-            } else if (array[m] > target) {
-                j = m - 1;
-            } else {
+            } else if (array[m] < target) {
                 i = m + 1;
+            } else {
+                j = m - 1;
             }
         }
         return -1;
     }
 
-    // recursion
-    private static int binarySearch2(int[] array, int target) {
-        return binary(array, 0, array.length - 1, target);
+    // 递归实现
+    private int search_recursion(int[] array, int target) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+        return search_recursion_core(array, 0, array.length - 1, target);
     }
-
-    private static int binary(int[] array, int i, int j, int target) {
+    // 递归核心：入参：数组，索引范围，目标值，返回值：索引
+    private int search_recursion_core(int[] array, int i, int j, int target) {
         // 递归出口
         if (i > j) {
             return -1;
         }
-        int m = i + (j - i) / 2;
+        int m = (i + j) / 2;
         if (array[m] == target) {
             return m;
         }
-        // 更新参数
-        if (array[m] > target) {
-            j = m -1;
+        // 更新参数，调用自身
+        if (array[m] < target) {
+            return search_recursion_core(array, m + 1, j, target);
         } else {
-            i = m + 1;
+            return search_recursion_core(array, i, m - 1, target);
         }
-        // 调用自身
-        return binary(array, i, j, target);
     }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        BinarySearch binarySearch = new BinarySearch();
+        System.out.println(binarySearch.search_iteration(array, 5));
+        System.out.println(binarySearch.search_recursion(array, 5));
+    }
+
 }
